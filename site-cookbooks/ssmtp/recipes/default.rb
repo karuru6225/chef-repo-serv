56919f42f4fbd770cf5e53fdd67d67b57e5a438f
karuru6225 <e2hjhi@gmail.com>
2014-08-15 04:53:47 +0900

    ssmtpの設定完了

--- site-cookbooks/ssmtp/recipes/default.rb
+++ site-cookbooks/ssmtp/recipes/default.rb
@@ -6,3 +6,26 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'ssmtp' do
+	action :install
+end
+
+
+settings = Chef::EncryptedDataBagItem.load("ssmtp", "settings");
+template '/etc/ssmtp/ssmtp.conf' do
+	source 'ssmtp.conf.erb'
+	owner 'root'
+	group 'root'
+	mode '0644'
+	variables({
+		:email => settings['email'],
+		:smtp => settings['smtp'],
+		:hostname => settings['hostname'],
+		:username => settings['username'],
+		:password => settings['password'],
+		:usestarttls => settings['usestarttls']
+	})
+end
+
+
