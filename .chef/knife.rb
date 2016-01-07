970c79f6d39fc8ab450e3c6524078d90b382cefd
karuru6225 <e2hjhi@gmail.com>
2016-01-08 08:39:27 +0900

    足りないファイル／ディレクトリを追加

---
+++ .chef/knife.rb
@@ -0,0 +1,5 @@
+file_cache_path "/tmp/chef-solo"
+cookbook_path ["/root/chef-repo-serv/cookbooks","/root/chef-repo-serv/site-cookbooks"]
+data_bag_path "/root/chef-repo-serv/data_bags"
+role_path "/root/chef-repo-serv/roles"
+encrypted_data_bag_secret "/root/chef-repo-serv/data_bag_key"
